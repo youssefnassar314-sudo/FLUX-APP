26f16d13b82fb64365f719b9fc9540ac676c17cf
youssefnassar314-sudo <youssefnassar314@gmail.com>
2026-04-01 00:03:45 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'flux-os-v1';
+const assetsToCache = [
+    './',
+    './index.html',
+    './style.css',
+    './app.js',
+    './manifest.json',
+    './icons/icon-192.png',
+    './icons/icon-512.png'
+];
+
+// Pagka-install, i-save sa cache yung mga files
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(assetsToCache);
+        })
+    );
+});
+
+// Pag may ni-load ang app, i-check muna kung nasa cache para mabilis
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
